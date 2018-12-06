295d57c8b9ca8a1b60eab76fd43f1304335215ea
hongyx11 <yuxi>
2018-12-06 18:10:17 +0800

    'update'

--- psycoding/GUI/main.py
+++ psycoding/GUI/main.py
@@ -1,10 +1,15 @@
-import sys, os, json, multiprocessing, collections
+import sys, os, json, multiprocessing, collections, shutil, time, copy
 
 from PyQt5.QtWidgets import (QWidget, QMainWindow, QAction, 
             qApp, QApplication, QTextEdit, QVBoxLayout, QHBoxLayout, 
-            QPushButton, QScrollArea, QFileDialog, QMessageBox)
+            QPushButton, QScrollArea, QFileDialog, QMessageBox, QLineEdit)
 from PyQt5.QtGui import QIcon, QFont, QPixmap
-import jieba
+import jieba, snownlp
+from whoosh.index import create_in, open_dir
+from whoosh.fields import *
+from whoosh.qparser import QueryParser
+from jieba.analyse.analyzer import ChineseAnalyzer
+
 sys.path.append(".")
 from psycoding.ffmpeg.psy_ffmpeg import psy_ffmpeg
 from psycoding.xunfei import xunfei
@@ -76,8 +81,10 @@ class mainwidget(QWidget):
         self.videopath = ""
         self.savefilename = ""
         self.__textbox = []
-        self.__textboxindex = 0
+        self.__textboxnum = 0
         self.__wordset = set()
+        self.searchengine_inited = False
+        self.searchboxadded = -1
         self.initUI()
 
     def getfilename(self):
@@ -104,6 +111,11 @@ class mainwidget(QWidget):
                 self.__wordset.add(line.decode('utf8').strip())
         print(self.__wordset)
         
+    def uploadresult(self):
+        """
+        upload results
+        """
+        pass
 
     def audio2text_fortestuse(self):
         inputstr = xfins.audio2text(0)
@@ -140,8 +152,8 @@ class mainwidget(QWidget):
                 f.writelines(inputstr + "\n")
 
     def load_processed_data(self):
-        self.__textboxindex = []
-        self.__textboxindex = 0
+        self.__textbox = []
+        self.__textboxnum = 0
         processed_data_path = QFileDialog.getOpenFileName()[0]
         if not processed_data_path:
             return 
@@ -149,15 +161,18 @@ class mainwidget(QWidget):
         with open(processed_data_path, 'r') as f:
             while 1:
                 line = f.readline()
+                line += f.readline()    
+                textfile_list += [line]
                 if not line:
                     break
-                textfile_list += [line]
-        self.addtextboxlist(textfile_list)
-
+        print(textfile_list)
+        self.addtextboxlist(textfile_list[:-1])
 
     def savefile(self):
         if self.savefilename == "":
             self.savefilename = QFileDialog.getSaveFileName()[0]
+        if not self.savefilename:
+            return 
         linecnt = 0
         with open(self.savefilename, 'w') as f:
             for i in range(len(self.__textbox)):
@@ -165,14 +180,12 @@ class mainwidget(QWidget):
                 if not content.startswith("defaultdict"):
                     f.writelines(content + "\n")
 
-    
-
     def saveas(self):
-        self.savefilename = QFileDialog.getSaveFileName()
+        self.savefilename = QFileDialog.getSaveFileName()[0]
         savefile()
 
     def addtextboxlist(self, inputstrlist=None):
-        self.__textboxindex += len(inputstrlist)
+        self.__textboxnum += len(inputstrlist)
         for s in inputstrlist:
             tb = QTextEdit()
             if not s:
@@ -186,7 +199,7 @@ class mainwidget(QWidget):
         self.show()
 
     def addtextbox(self, inputstr=None):
-        self.__textboxindex += 1
+        self.__textboxnum += 1
         tb = QTextEdit()
         if not inputstr:
             tb.setText("no input")
@@ -208,7 +221,10 @@ class mainwidget(QWidget):
         """
         Method of Analyze -> matchwords
         """
-        for i in range(len(self.__textbox)):
+        self.clearanalysis()
+        self.addtextbox("Match Results")
+        for i in range(self.__textboxnum-1):
+            print(i)
             context = self.__textbox[i].toPlainText()
             resdict = collections.defaultdict(int)
             for t in self.__wordset:
@@ -218,14 +234,106 @@ class mainwidget(QWidget):
                 continue
             self.addtextbox(str(resdict) + context)
 
-    def cutwords(self):
-        pass
+    def init_searchwords(self):
+        analyzer = ChineseAnalyzer()
+        schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True, analyzer=analyzer))
+        if not os.path.exists("tmp"):
+            os.mkdir("tmp")
+        else:
+            shutil.rmtree("tmp")
+            time.sleep(1)
+            os.mkdir("tmp")
+        ix = create_in("tmp", schema) # for create new index
+        writer = ix.writer()
+        for tb in self.__textbox:
+            context = tb.toPlainText()
+            if context.startswith('default'):
+                continue
+            print(context.split(':')[-1])
+        
+            #context.split(':')[-1]
+            writer.add_document(
+                title=context.split(':')[0],
+                path = '/',
+                content = context.split(':')[-1]
+            )
+        writer.commit()
+        self.searcher = ix.searcher()
+        self.parser = QueryParser("content", schema=ix.schema)
+        self.searchengine_inited = True
+    
+    def removetextbox(self,ind):
+        # remove the textbox after ind (inclusive)
+        for i in range(ind, self.__textboxnum):
+            self.vbox.removeWidget(self.__textbox[i])
+        self.__textbox = self.__textbox[:ind]
+        self.__textboxnum = len(self.__textbox)
+        
+
+    def searchwords(self):
+        self.clearanalysis()
+        cnt = -1
+        for i in range(self.__textboxnum):
+            if self.__textbox[i].toPlainText().startswith("Search input"):
+                print(i)
+                cnt = i
+                break
+        print(cnt)
+        if cnt < 0:
+            self.addtextbox("Search input:")
+            return
+
+        searchword = self.__textbox[-1].toPlainText().split(":")[-1]
+        print(searchword)
+        if not self.searchengine_inited:
+            self.init_searchwords()
+        q = self.parser.parse(searchword)
+        results = self.searcher.search(q)
+        for hit in results:
+            ansstr = "Search Results:\n"
+            print(hit.highlights("content"))
+            print(hit.highlights("title"))
+            ansstr = ansstr + hit.highlights("title") + "\n"
+            ansstr = ansstr + hit.highlights("content") + "\n"
+            self.addtextbox(ansstr)
+
+    def clearanalysis(self):
+        cnt = 0
+        print(self.__textboxnum)
+        for i in range(self.__textboxnum):
+            j1 = self.__textbox[i].toPlainText().startswith("Sentiments") 
+            j2 = self.__textbox[i].toPlainText().startswith("Search Results")
+            j3 = self.__textbox[i].toPlainText().startswith("Match Results")
+            if j1 or j2 or j3:
+                print(j1,j2,j3,i)
+                cnt = i
+                break
+        if cnt != 0:
+            print("clear ana cnt is {}".format(cnt))
+            self.removetextbox(cnt)
 
     def saanalysis(self):
         """
         sentimental analysis
         """
-        pass
+        self.clearanalysis()
+        endindex = self.searchboxadded if self.searchboxadded > 0 else self.__textboxnum
+        print(endindex)
+        if endindex <= 0:
+            return
+        res = ["Sentiments Aanylsis Results:"]
+        for i in range(endindex):
+            context = self.__textbox[i].toPlainText().split(":")[-1]
+            if len(context) == 0:
+                continue
+            pos_p = snownlp.SnowNLP(context).sentiments
+            print(pos_p)
+            res.append("{} - {} min's positive sentiment probability is {}.".format(i, i+1, pos_p))
+        finalstr = "\n".join(res)
+        self.addtextbox(finalstr)
+            
+            
+
 
 class Example(QMainWindow):
 
@@ -234,19 +342,6 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        # QToolTip.setFont(QFont('SansSerif', 10))
-        # self.setToolTip('This is a <b>QWidget</b> widget')
-        # btn = QPushButton('Button', self)
-        # btn.setToolTip('This is a <b>QPushButton</b> widget')
-        # btn.resize(btn.sizeHint())
-        # #btn.clicked.connect(QCoreApplication.instance().quit)
-        # btn.move(50,50)
-        # self.setGeometry(100,100,300,300)
-        # self.setWindowTitle("Icon")
-        # #self.setWindowIcon(QIcon('C:\\Users\\hongy\\Documents\\work\\cici\\BehavioralCoding-cs410project\\psycoding\\GUI\\icon.png'))
-        # self.setWindowIcon(QIcon('/Users/yuxi/work/cici/BehavioralCoding-cs410project/psycoding/GUI/icon.jpg'))
-        # self.show()
-
         mw = mainwidget()
 
         # File -> Load video 
@@ -281,6 +376,12 @@ class Example(QMainWindow):
         matchwordsAction.setStatusTip('matchwords')
         matchwordsAction.triggered.connect(mw.matchwords)
         
+        # Analyze -> cutwords
+        searchwordsAction = QAction('&searchwords', self)       
+        searchwordsAction.setStatusTip('searchwords')
+        searchwordsAction.triggered.connect(mw.searchwords)
+        
+
         # Analyze -> sentimental analysis
         saAction = QAction('&sentimental analysis', self)       
         saAction.setStatusTip('sentimental analysis')
@@ -305,6 +406,7 @@ class Example(QMainWindow):
         # menubar -> analyze
         AnalyzeMenu = menubar.addMenu('Analyze')
         AnalyzeMenu.addAction(matchwordsAction)
+        AnalyzeMenu.addAction(searchwordsAction)
         AnalyzeMenu.addAction(saAction)
         configffmpegAction = QAction("ffmpegpath",self)
         configffmpegAction.setStatusTip("config ffmpeg bin path")
@@ -332,19 +434,6 @@ class Example(QMainWindow):
 
 
 if __name__ == "__main__":
-    # print(os.getcwd())
-    # rootdir = os.path.join(os.getcwd(),'config.json')
-    # with open(rootdir,'r') as f:
-    #     jdata = json.loads(f.read())
-    #     print(jdata)
-    #     print(jdata['rootfile'])
-        #print(f.read())
-    #print(os.path.dirname(os.getcwd()))
-    #testcls = configcls(configpath=r"C:\Users\hongy\Documents\work\cici\BehavioralCoding-cs410project\psycoding\GUI\main.py")
     app = QApplication(sys.argv)
-    #app.setWindowIcon(QIcon('./apple.ico'))
-    
     ins = Example()
     sys.exit(app.exec_())
-    #ffmpegcls = psy_ffmpeg()
-    #ffmpegcls.VideoToAudio(videofilefullpath = "C:\\Users\\hongy\\Documents\\work\\cici\\ffmpeg-4.0.2-win64\\bin\\codingexample.mov")
